92e8327c009990c2d9c2d84a2904e86b5200bfe7
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-07-10 23:11:46 +0100

    Added left animation

---
+++ New Unity Project/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Animator animator;
+    private Transform target;
+    [SerializeField]
+    private float speed = 2;
+    [SerializeField]
+    private float range;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        //target = FindObjectOfType<PlayerController>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        followPlayer();
+    }
+
+    void followPlayer(){
+        animator.SetBool("isMoving", true);
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+}
