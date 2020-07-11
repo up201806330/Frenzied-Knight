f7fb1b0571f51c41f20808d4db9c30b4a0f9263f
Desward-games <67900079+Desward-games@users.noreply.github.com>
2020-07-11 09:21:25 +0100

    knight spear animations, fixed diagonal speed

---
+++ GMTK2020/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Animator animator;
+    private CharacterController target;
+    [SerializeField]
+    private float speed = 100f;
+
+    private bool m_FacingRight = true;  // For determining which way the enemy is currently facing.
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        followPlayer();
+    }
+
+    void followPlayer(){
+        //if (target.alive) animator.SetBool("playerDead", false);
+        //else
+        //{
+        //    animator.SetBool("playerDead", true);
+        //    return;
+        //}
+
+        float horizontalMove = target.transform.position.x - transform.position.x;
+        if ((horizontalMove > 0 && !m_FacingRight) || (horizontalMove < 0 && m_FacingRight)) Flip();
+        
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ GMTK2020/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 200f;
+	private Rigidbody2D rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	void Update()
+	{
+		float horizontalMove = Input.GetAxisRaw("Horizontal") * speed, verticalMove = Input.GetAxis("Vertical") * speed;
+		if(horizontalMove == 0 || verticalMove == 0)
+		{
+			rb.velocity = new Vector2(horizontalMove * Time.fixedDeltaTime, verticalMove * Time.fixedDeltaTime);
+		} else
+		{
+			rb.velocity = new Vector2(0.7f * horizontalMove * Time.fixedDeltaTime, 0.7f * verticalMove * Time.fixedDeltaTime);
+		}
+		
+	}
+}
