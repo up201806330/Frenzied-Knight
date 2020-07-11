c99097d5a93074db2ee220f48a8eaab3c70d97c8
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-07-11 13:51:51 +0100

    Restructured files ;

--- GMTK2020/Assets/Scripts/EnemyController.cs
+++ GMTK2020/Assets/Scripts/EnemyController.cs
@@ -1,13 +1,10 @@
 ﻿using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class EnemyController : CharacterController
 {
-    private Animator animator;
     private GameObject target;
     [SerializeField]
     private float speed = 100f;
-
-    private bool m_FacingRight = true;  // For determining which way the enemy is currently facing.
 
     void Start()
     {
@@ -21,12 +18,12 @@ public class EnemyController : MonoBehaviour
     }
 
     void followPlayer(){
-        //if (target.alive) animator.SetBool("playerDead", false);
-        //else
-        //{
-        //    animator.SetBool("playerDead", true);
-        //    return;
-        //}
+        if (!target.GetComponent<KnightController>().alive) animator.SetBool("playerDead", false);
+        else
+        {
+            animator.SetBool("playerDead", true);
+            return;
+        }
 
         float horizontalMove = target.transform.position.x - transform.position.x;
         if ((horizontalMove > 0.8f && !m_FacingRight) || (horizontalMove < -0.8f && m_FacingRight)) Flip();
@@ -34,14 +31,5 @@ public class EnemyController : MonoBehaviour
         transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
     }
 
-    private void Flip()
-    {
-        // Switch the way the player is labelled as facing.
-        m_FacingRight = !m_FacingRight;
-
-        // Multiply the player's x local scale by -1.
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-    }
+    
 }
---
+++ GMTK2020/Assets/Scripts/KnightController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightController : CharacterController
+{
+    public bool alive = true;
+
+    protected override void Flip()
+    {
+        base.Flip();
+
+        // So that the knight body stays on the same position when getting flipped with the spear, instead of flipping the entire sprite.
+        // Not optimal but faster than making the spear a game object on its own.
+        if (gameObject.name == "Knight")
+        {
+            if (m_FacingRight) transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+            else if (!m_FacingRight) transform.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z); ;
+        }
+    }
+}
