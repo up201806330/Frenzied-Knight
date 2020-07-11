d6931dbc535970be84e16e4e3fcd072f8a2e9ca4
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-07-11 01:45:47 +0100

    Temporary player ; Enemy tracking and animations finished

--- New Unity Project/Assets/Scripts/EnemyController.cs
+++ New Unity Project/Assets/Scripts/EnemyController.cs
@@ -1,32 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
     private Animator animator;
-    private Transform target;
+    private PlayerController target;
     [SerializeField]
-    private float speed = 2;
-    [SerializeField]
-    private float range;
+    private float speed;
 
+    private bool m_FacingRight = true;  // For determining which way the enemy is currently facing.
 
-    // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        //target = FindObjectOfType<PlayerController>().transform;
+        target = FindObjectOfType<PlayerController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         followPlayer();
     }
 
     void followPlayer(){
-        animator.SetBool("isMoving", true);
-        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        if (target.alive) animator.SetBool("playerDead", false);
+        else
+        {
+            animator.SetBool("playerDead", true);
+            return;
+        }
+
+        float horizontalMove = target.transform.position.x - transform.position.x;
+        if ((horizontalMove > 0 && !m_FacingRight) || (horizontalMove < 0 && m_FacingRight)) Flip();
+        
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+
+	public Animator animator;
+
+	public float speed;
+	[SerializeField]
+	public bool alive = true;
+
+    void Update()
+    {
+		float horizontalMove = Input.GetAxisRaw("Horizontal") * speed, verticalMove = Input.GetAxis("Vertical") * speed;
+		if ((horizontalMove > 0 && !m_FacingRight) || (horizontalMove < 0 && m_FacingRight)) Flip();
+
+		if (horizontalMove == 0 && verticalMove == 0) animator.SetBool("isMoving", false);
+		else animator.SetBool("isMoving", true);
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalMove * Time.fixedDeltaTime, verticalMove * Time.fixedDeltaTime);
+	}
+
+	private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		m_FacingRight = !m_FacingRight;
+
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
