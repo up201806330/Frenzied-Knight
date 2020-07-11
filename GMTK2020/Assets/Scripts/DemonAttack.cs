5f8251fc797a8f034e2e11b7d4615905b707a6c1
Desward-games <67900079+Desward-games@users.noreply.github.com>
2020-07-11 17:30:20 +0100

    demon attack

--- GMTK2020/Assets/Scripts/DemonAttack.cs
+++ GMTK2020/Assets/Scripts/DemonAttack.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class DemonAttack : MonoBehaviour
 {
+    private float fixedTimer = 0.2f;
+    private float timer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = fixedTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime; //dash cooldown goes down
+
+        //if player presses spacebar
+        if(Input.GetButtonDown("Attack") && timer <= 0)
+        {
+            GetComponent<Animator>().SetTrigger("Attacking");
+            Shoot();
+            timer = fixedTimer; //reset the timer
+        }
+    }
+
+    //spawn a fireball
+    private void Shoot()
+    {
+        Instantiate(Resources.Load("Prefabs/Fireball"), transform.position, transform.rotation);
     }
 }
---
+++ GMTK2020/Assets/Scripts/Fireball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public int attack = 2;
+    public int speed = 500;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        transform.localScale = GameObject.FindGameObjectWithTag("Player").transform.localScale;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(transform.localScale.normalized.x * speed * Time.fixedDeltaTime, 0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            other.gameObject.GetComponent<EnemyHealth>().DealDamageToEnemy(attack);
+            Destroy(gameObject);
+        }
+    }
+}
